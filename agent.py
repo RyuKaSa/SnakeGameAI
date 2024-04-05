20d7b2b1f38a0ecaaf748e14f075e821d180f28b
William Bogdanovic <william.bogdanovic@yahoo.fr>
2024-04-05 15:14:51 +0200

    adding plots and calling helper.py

--- agent.py
+++ agent.py
@@ -19,6 +19,7 @@ from collections import deque
 # from local files
 from game import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
+from helper import plot
 
 # params
 MAX_MEMORY = 100_000
@@ -144,6 +145,12 @@ def train():
                 agent.model.save()
             
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
 if __name__ == '__main__':
     train()
